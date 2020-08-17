8da6534826bbcbe6ccdb78250c645eb69c71d5a8
Tsahi Asher <tsahi_75@yahoo.com>
2020-08-17 20:55:20 +0300

    spelling errors

--- source/Src/Data/Configuration/DatabaseSyntheticConfigSettings.cs
+++ source/Src/Data/Configuration/DatabaseSyntheticConfigSettings.cs
@@ -47,7 +47,7 @@ namespace Microsoft.Practices.EnterpriseLibrary.Data.Configuration
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DatabaseSyntheticConfigSettings"/> class
-        /// with the given configuraiton access function.
+        /// with the given configuration access function.
         /// </summary>
         public DatabaseSyntheticConfigSettings(Func<string, ConfigurationSection> configurationSource)
         {
