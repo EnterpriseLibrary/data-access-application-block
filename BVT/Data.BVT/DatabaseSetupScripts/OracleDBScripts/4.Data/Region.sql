30fe2905919c6458f04b431fba8ce2bdb1ef9054
Tsahi Asher <tsahi_75@yahoo.com>
2020-08-30 11:03:47 +0300

    add Region table

---
+++ BVT/Data.BVT/DatabaseSetupScripts/OracleDBScripts/4.Data/Region.sql
@@ -0,0 +1,8 @@
+BEGIN
+INSERT INTO Region VALUES (1,'Eastern');
+INSERT INTO Region VALUES (2,'Western');
+INSERT INTO Region VALUES (3,'Northern');
+INSERT INTO Region VALUES (4,'Southern');
+COMMIT;
+END;
+/
